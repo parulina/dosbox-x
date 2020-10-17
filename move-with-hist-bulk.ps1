6c3ba7ec7bb16c18ba109b6878e124274c1cc260
aybe <aybe.one@hotmail.com>
2020-10-17 08:59:15 +0200

    Update move-with-hist-bulk.ps1

--- move-with-hist-bulk.ps1
+++ move-with-hist-bulk.ps1
@@ -12,12 +12,16 @@ Write-Host("Source is {0}" -f $source) -ForegroundColor Green
 Write-Host("Target is {0}" -f $target) -ForegroundColor Green
 
 $files = Get-ChildItem -path $source -Recurse -Attributes !Directory | Resolve-Path -Relative
+$index = 0
+$count = $files.Length
 
 foreach($file in $files)
 {
     $dest = "{0}{1}" -f $target, $file.Substring(1)
 
-    Write-Host("Moving '{0}' to '{1}'" -f $file, $dest) -BackgroundColor Cyan -ForegroundColor White
+    Write-Host("Processing file $($index + 1) of $count".PadRight(80)) -BackgroundColor Cyan -ForegroundColor White
+    Write-Host("Source = $file".PadRight(80)) -BackgroundColor Cyan -ForegroundColor White
+    Write-Host("Target = $dest".PadRight(80)) -BackgroundColor Cyan -ForegroundColor White
     
     .\move-with-hist.ps1 $file $dest
 
@@ -26,4 +30,6 @@ foreach($file in $files)
         Write-Host("ERROR : `$LASTEXITCODE = $LASTEXITCODE") -BackgroundColor Red -ForegroundColor White
         exit
     }
+
+    $index++
 }
